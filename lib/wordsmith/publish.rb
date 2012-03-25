a55a7ea2ad32c66c0a3a6fe906621f477b516813
Javier Saldana <javier@tractical.com>
2012-03-25 03:58:24 -0600

    fix path in #publish

--- lib/wordsmith/publish.rb
+++ lib/wordsmith/publish.rb
@@ -19,14 +19,14 @@ class Wordsmith
 
     # publish compiled html files to a
     # Github project page
-    def publish
-      from_path = if File.exists?(File.join(local('final', "#{@name}_html")))
-        File.join(local('final', "#{@name}_html"))
-      elsif File.exists?(File.join(local('final', "#{@name}.html")))
-        File.join(local('final', "#{@name}.html"))
+    def publish(from_path='')
+      from_path = if File.exists?(local(File.join('final', "#{@name}_html")))
+        local(File.join('final', "#{@name}_html"))
+      elsif File.exists?(local(File.join('final', "#{@name}.html")))
+        local(File.join('final', "#{@name}.html"))
       end
       
-      if from_path.blank?
+      if from_path == ''
         raise "Exiting.. Nothing to publish. Have you run 'wordsmith generate'?"
       end
 
