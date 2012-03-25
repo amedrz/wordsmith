1d4b344dd733e9e28d03221b755e6e15a845486d
Amed Rodriguez <amed@tractical.com>
2012-03-24 18:30:30 -0600

    fix spec, add bin file

--- spec/wordsmith/cli_spec.rb
+++ spec/wordsmith/cli_spec.rb
@@ -3,7 +3,7 @@ require "wordsmith/cli"
 describe Wordsmith::CLI do
 
   it "runs" do
-    lambda{ Wordsmith.new.publish }.should_not raise_error
+    lambda{ Wordsmith.new.run }.should_not raise_error
   end
 
   it "runs publish" do
