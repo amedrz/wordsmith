c004f33e9b2e5949bdaca454744d3caceca15096
Javier Saldana <javier@tractical.com>
2013-04-15 00:14:04 -0500

    Add tests for generate cmd

--- test/generate_test.rb
+++ test/generate_test.rb
@@ -4,4 +4,34 @@ context "wordsmith generate tests" do
   setup do
     @wordsmith = Wordsmith.new
   end
+
+  test "fails without files" do
+    assert_raise RuntimeError do
+      @wordsmith.generate
+    end
+  end
+
+  %w(epub html pdf).each do |format|
+    test "generates book.#{format}" do
+      in_temp_dir do
+        @wordsmith.init "book"
+        Dir.chdir("book") { Wordsmith.new.generate [format] }
+      end
+    end
+  end
+
+  # mobi requires epub
+  test "generates book.mobi" do
+    in_temp_dir do
+      @wordsmith.init "book"
+      Dir.chdir("book") { Wordsmith.new.generate ["epub", "mobi"] }
+    end
+  end
+
+  test "generates all" do
+    in_temp_dir do
+      @wordsmith.init "book"
+      Dir.chdir("book") { Wordsmith.new.generate }
+    end
+  end
 end
