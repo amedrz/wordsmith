97a356ade249323de76b7ef9d13887615afc8a56
Javier Saldana <javier@tractical.com>
2013-05-08 14:38:11 -0500

    Fix stylesheet handling

--- test/css_test.rb
+++ test/css_test.rb
@@ -6,44 +6,47 @@ context "wordsmith css tests" do
     Wordsmith.new
   end
 
-  test "includes all stylesheets into html" do
+  test "copies all css files into final assets" do
     in_temp_dir do
       wordsmith.init('w')
       Dir.chdir("w") { wordsmith.generate ["html"] }
-      files = Dir.glob('w/**/*')
+
+      files = Dir.glob('w/final/**/*')
+
       assert files.include? "w/final/w/assets/stylesheets/master.css"
       assert files.include? "w/final/w/assets/stylesheets/other.css"
-      html_contents = File.read("w/final/w/index.html")
-      assert html_contents =~ /master.css/
-      assert html_contents =~ /other.css/
     end
   end
 
-  test "ignores css files starting with _" do
+  test "includes all css files inside final html" do
     in_temp_dir do
       wordsmith.init('w')
-      FileUtils.cp("w/assets/stylesheets/master.css",
-        "w/assets/stylesheets/_partial.css")
+
+      File.rename "w/assets/stylesheets/master.scss",
+        "w/assets/stylesheets/master.css"
+
+      assert File.exists?("w/assets/stylesheets/other.css")
+
       Dir.chdir("w") { wordsmith.generate ["html"] }
-      files = Dir.glob('w/**/*')
-      assert files.include? "w/final/w/assets/stylesheets/master.css"
-      assert files.include? "w/final/w/assets/stylesheets/_partial.css"
-      html_contents = File.read("w/final/w/index.html")
-      assert html_contents =~ /master.css/
-      assert !(html_contents =~ /_partial.css/)
+      html_content = File.read("w/final/w/index.html")
+
+      assert html_content =~ /master.css/
+      assert html_content =~ /other.css/
     end
   end
 
   test "ignores epub.css in html" do
     in_temp_dir do
       wordsmith.init('w')
-      FileUtils.cp("w/assets/stylesheets/master.css",
-        "w/assets/stylesheets/epub.css")
+      assert File.exists?("w/assets/stylesheets/epub.css")
+
       Dir.chdir("w") { wordsmith.generate ["html"] }
-      files = Dir.glob('w/**/*')
+
+      html_content = File.read("w/final/w/index.html")
+      files = Dir.glob('w/final/**/*')
+
       assert !files.include?("w/final/w/assets/stylesheets/epub.css")
-      html_contents = File.read("w/final/w/index.html")
-      assert !(html_contents =~ /epub.css/)
+      assert !(html_content =~ /epub.css/)
     end
   end
 
